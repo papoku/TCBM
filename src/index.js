fe504e026fd208bea6b1112a436c9d4bdddb004e
papoku <pongku.kumar.paul@gmail.com>
2019-04-08 22:10:38 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*main page that renders
+when the site gets first hit */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './App';
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
